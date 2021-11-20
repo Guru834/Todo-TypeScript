2600d4cd10acd1d2bd4c384a6bb12c13ed0d2185
Guruansh Singh <gurus5229@gmail.com>
2021-11-20 17:43:04 -0500

    Working -> No Beautification

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, TextField } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useState } from "react";
+import Todo from "./components/Todo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface IProps {
+  name: string;
 }
+const App = () => {
+  const [todoName, setTodoName] = useState<string>("");
+  const [todoList, setTodoList] = useState<IProps[]>([]);
+  const handleChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ) => {
+    setTodoName(e.target.value);
+  };
+  const handleCreate = () => {
+    const todo = { name: todoName };
+    setTodoList([...todoList, todo]);
+  };
+  const handleDelete = (selected: string): void => {
+    setTodoList(
+      todoList.filter((item) => {
+        return item.name !== selected;
+      })
+    );
+  };
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column" }}>
+      <Box
+        sx={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "5%",
+        }}
+      >
+        <TextField
+          id="outlined-basic"
+          label="Enter Todo"
+          variant="outlined"
+          onChange={handleChange}
+          sx={{
+            width: "50%",
+            backgroundColor: "white",
+            borderRadius: "10px",
+          }}
+        />
+        <Box>
+          <Button
+            variant="contained"
+            onClick={handleCreate}
+            size="large"
+            sx={{ height: "55px", marginLeft: "20%" }}
+          >
+            Create
+          </Button>
+        </Box>
+      </Box>
+      <Box sx={{ marginTop: "4%" }}>
+        {todoList.map((item: IProps, key: number) => {
+          return <Todo todoList={item} key={key} handleDelete={handleDelete} />;
+        })}
+      </Box>
+    </Box>
+  );
+};
 
 export default App;
---
+++ my-app/src/components/Todo.tsx
@@ -0,0 +1,50 @@
+import { Checkbox, Grid } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+import { IProps } from "../App";
+import CancelIcon from "@mui/icons-material/Cancel";
+
+interface ITodo {
+  todoList: IProps;
+  handleDelete(selected: string): void;
+}
+const Todo: React.FC<ITodo> = ({ todoList, handleDelete }) => {
+  const handleClick = () => {
+    handleDelete(todoList.name);
+    console.log(todoList.name);
+  };
+  return (
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <Box sx={{ width: "50%" }}>
+        <Grid
+          container
+          item
+          xs={12}
+          sx={{
+            display: "flex",
+            textAlign: "center",
+            alignItems: "center",
+            marginTop: "2%",
+            border: "1px solid black",
+          }}
+        >
+          <Grid item xs={4}>
+            <Checkbox />
+          </Grid>
+          <Grid
+            item
+            xs={2}
+            sx={{ display: "flex", justifyContent: "flex-start" }}
+          >
+            {todoList.name}
+          </Grid>
+          <Grid item xs={6}>
+            <CancelIcon onClick={handleClick} />
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
+};
+
+export default Todo;
